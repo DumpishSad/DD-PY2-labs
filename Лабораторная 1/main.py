e10fc6b6c760e4ed47a2bd47266339a37d56e1b9
DumpishSad <121685754+DumpishSad@users.noreply.github.com>
2023-12-17 21:30:02 +0300

    Update main.py

--- Лабораторная 1/main.py
+++ Лабораторная 1/main.py
@@ -8,6 +8,10 @@ class Transport:
         :param model: Модель транспортного средства.
         :param year: Год выпуска транспортного средства.
         """
+        if not isinstance(brand, str) or not isinstance(model, str) or not isinstance(year, int):
+            raise TypeError("Неверный тип данных для одного или нескольких параметров.")
+        if year > 2023:
+            raise ValueError("Год выпуска не может быть больше 2023!")
         self.brand = brand
         self.model = model
         self.year = year
@@ -17,23 +21,25 @@ class Transport:
         Запускает двигатель транспортного средства.
 
         :return: Строка с сообщением о запуске двигателя.
-
-        >>> car = Transport("Toyota", "Camry", 2022)
-        >>> car.start_engine()
-        'Двигатель машины Toyota Camry запущен.'
+        Examples:
+            >>> car = Transport("Toyota", "Camry", 2022)
+            >>> car.start_engine()
+            'Двигатель машины Toyota Camry запущен.'
         """
         return f'Двигатель машины {self.brand} {self.model} запущен.'
 
     def drive(self, distance: float) -> str:
+        if not isinstance(distance, int):
+            raise TypeError("Неверный тип данных")
         """
         Осуществляет поездку на заданное расстояние.
 
         :param distance: Расстояние в километрах.
         :return: Строка с сообщением о поездке.
-
-        >>> bike = Transport("Harley-Davidson", "Sportster", 2021)
-        >>> bike.drive(50.5)
-        'Проехали 50.5 км на Harley-Davidson Sportster.'
+        Examples:
+            >>> bike = Transport("Harley-Davidson", "Sportster", 2021)
+            >>> bike.drive(50.5)
+            'Проехали 50.5 км на Harley-Davidson Sportster.'
         """
         return f'Проехали {distance} км на {self.brand} {self.model}.'
 
@@ -42,10 +48,10 @@ class Transport:
         Останавливает двигатель транспортного средства.
 
         :return: Строка с сообщением об остановке двигателя.
-
-        >>> bus = Transport("Mercedes-Benz", "Sprinter", 2020)
-        >>> bus.stop_engine()
-        'Двигатель Mercedes-Benz Sprinter остановлен.'
+        Examples:
+            >>> bus = Transport("Mercedes-Benz", "Sprinter", 2020)
+            >>> bus.stop_engine()
+            'Двигатель Mercedes-Benz Sprinter остановлен.'
         """
         return f'Двигатель {self.brand} {self.model} остановлен.'
 
@@ -60,6 +66,8 @@ class Book:
         :param genre: Жанр книги.
         :param year_published: Год публикации книги.
         """
+        if not all(isinstance(param, str) for param in (title, author, genre)) or not isinstance(year_published, int):
+            raise TypeError("Неверный тип данных для одного или нескольких параметров.")
         self.title = title
         self.author = author
         self.genre = genre
@@ -70,10 +78,10 @@ class Book:
         Позволяет прочитать книгу.
 
         :return: Строка с сообщением о прочтении книги.
-
-        >>> novel = Book("Crime and Punishment", "Fyodor Dostoevsky", "Fiction", 1866)
-        >>> novel.read_book()
-        'Чтение книги "Crime and Punishment" автора Fyodor Dostoevsky.'
+        Examples:
+            >>> novel = Book("Crime and Punishment", "Fyodor Dostoevsky", "Fiction", 1866)
+            >>> novel.read_book()
+            'Чтение книги "Crime and Punishment" автора Fyodor Dostoevsky.'
         """
         return f'Чтение книги "{self.title}" автора {self.author}.'
 
@@ -81,12 +89,11 @@ class Book:
         """
         Рекомендует книгу другу.
 
-        :param friend: Имя друга.
-        :return: Строка с рекомендацией.
 
-        >>> mystery_book = Book("The Da Vinci Code", "Dan Brown", "Mystery", 2003)
-        >>> mystery_book.recommend_book("Alice")
-        'Рекомендую прочитать книгу "The Da Vinci Code" автора Dan Brown, Alice.'
+        Examples:
+            >>> mystery_book = Book("The Da Vinci Code", "Dan Brown", "Mystery", 2003)
+            >>> mystery_book.recommend_book("Alice")
+            'Рекомендую прочитать книгу "The Da Vinci Code" автора Dan Brown, Alice.'
         """
         return f'Рекомендую прочитать книгу "{self.title}" автора {self.author}, {friend}.'
 
@@ -95,10 +102,10 @@ class Book:
         Возвращает жанр книги.
 
         :return: Жанр книги.
-
-        >>> sci_fi_book = Book("Dune", "Frank Herbert", "Science Fiction", 1965)
-        >>> sci_fi_book.get_genre()
-        'Science Fiction'
+        Examples:
+            >>> sci_fi_book = Book("Dune", "Frank Herbert", "Science Fiction", 1965)
+            >>> sci_fi_book.get_genre()
+            'Science Fiction'
         """
         return self.genre
 
@@ -111,6 +118,8 @@ class BankAccount:
         :param account_holder: Владелец счета.
         :param balance: Начальный баланс счета (по умолчанию 0).
         """
+        if not isinstance(account_holder, str) or not isinstance(balance, (float, int)):
+            raise TypeError("Неверный тип данных для одного или нескольких параметров.")
         self.account_holder = account_holder
         self.balance = balance
 
@@ -120,11 +129,13 @@ class BankAccount:
 
         :param amount: Сумма для пополнения.
         :return: Строка с подтверждением пополнения.
-
-        >>> savings_account = BankAccount("John Doe", 500.0)
-        >>> savings_account.deposit(100.0)
-        'Счет владельца John Doe пополнен на 100.0 рублей. Текущий баланс: 600.0 рублей.'
+        Examples:
+            >>> savings_account = BankAccount("John Doe", 500.0)
+            >>> savings_account.deposit(100.0)
+            'Счет владельца John Doe пополнен на 100.0 рублей. Текущий баланс: 600.0 рублей.'
         """
+        if not isinstance(amount, (float, int)):
+            raise TypeError("Неверный тип данных для суммы пополнения.")
         self.balance += amount
         return f'Счет владельца {self.account_holder} пополнен на {amount} рублей. Текущий баланс: {self.balance} рублей.'
 
@@ -134,11 +145,13 @@ class BankAccount:
 
         :param amount: Сумма для снятия.
         :return: Строка с подтверждением снятия средств.
-
-        >>> checking_account = BankAccount("Alice Smith", 1000.0)
-        >>> checking_account.withdraw(200.0)
-        'Со счета владельца Alice Smith снято 200.0 рублей. Текущий баланс: 800.0 рублей.'
+        Examples:
+            >>> checking_account = BankAccount("Alice Smith", 1000.0)
+            >>> checking_account.withdraw(200.0)
+            'Со счета владельца Alice Smith снято 200.0 рублей. Текущий баланс: 800.0 рублей.'
         """
+        if not isinstance(amount, (float, int)):
+            raise TypeError("Неверный тип данных для суммы снятия.")
         if amount > self.balance:
             return 'Недостаточно средств на счете.'
         self.balance -= amount
@@ -149,10 +162,10 @@ class BankAccount:
         Возвращает текущий баланс счета.
 
         :return: Текущий баланс счета.
-
-        >>> account = BankAccount("Jane Doe", 1500.0)
-        >>> account.get_balance()
-        1500.0
+        Examples:
+            >>> account = BankAccount("Jane Doe", 1500.0)
+            >>> account.get_balance()
+            1500.0
         """
         return self.balance
 
